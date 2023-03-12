c2d44441aa308900ec50bf71c40c3ee0bd5f08ac
Mathieu C <mathieu.kamvity@gmail.com>
2023-03-12 18:32:13 +0100

    Update SQL

--- src/test/resources/create-addresses.sql
+++ src/test/resources/create-addresses.sql
@@ -1,1 +1,2 @@
+delete from address where address_id='1';
 insert into address (address_id,address_name,address,city,zipCode,country,customer_id) values ('1','home','12 street Fol','Lisate','13412','Lowae','123456');
--- src/test/resources/create-customer.sql
+++ src/test/resources/create-customer.sql
@@ -1,1 +1,2 @@
+delete from customer where id='123456';
 insert into customer (id,title,firstname,lastname,email) values ('123456','Mrs','Marie','Curie','marie.curie@email.org')
