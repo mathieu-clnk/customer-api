503cdd1d170614c56b89ca9e4069314c6b7c29d7
Mathieu C <mathieu.kamvity@gmail.com>
2023-03-21 21:19:16 +0100

    Openapi provide more example

--- src/main/java/com/kamvity/samples/cm/controller/CustomerController.java
+++ src/main/java/com/kamvity/samples/cm/controller/CustomerController.java
@@ -5,7 +5,10 @@ import com.kamvity.samples.cm.response.Response;
 import com.kamvity.samples.cm.service.CustomerService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -28,15 +31,33 @@ public class CustomerController {
     private ModelMapper modelMapper;
 
     @PostMapping(path = "/create",consumes = {MediaType.APPLICATION_JSON_VALUE})
-    @Operation(summary = "createCustomer",description = "Create a new customer")
-    @Parameter(name = "customer", schema = @Schema(implementation = Customer.class, example = "{ " +
+    @Operation(summary = "Create customer",description = "Create a new customer")
+    @Parameters(value = {
+            @Parameter(name = "customer", required = true, example = "{ " +
             "\"id\" : \"123456\"," +
             "\"firstname\" : \"Nkosi\"," +
             "\"lastname\" : \"Johnson\"," +
             "\"email\" : \"nkosi.johnson@email.org\"" +
-            "}"))
+            "}")
+    })
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Customer created successfully"),
+            @ApiResponse(responseCode = "200", description = "Customer created successfully", content = @Content(
+                    examples = @ExampleObject(
+                            name = "Success response",
+                            value = "{"+
+                                    "\"status\": \"success\","+
+                                    "\"errorMessage\": \"\"," +
+                                    "\"sensitiveMessage\": \"\"," +
+                                    "\"result\": {"+
+                                    "  \"id\": 123456,"+
+                                    "  \"title\": \"Mrs\","+
+                                    "  \"firstname\": \"Nina\","+
+                                    "  \"lastname\": \"Simone\","+
+                                    "  \"email\": \"nina.simone@email.org\""+
+                                    "}"+
+                                    "}"
+                    )
+            )),
             @ApiResponse(responseCode = "400", description = "Error while creating the customer.")
     })
     public ResponseEntity<Response<Customer>> createCustomer(@RequestBody Customer customer) {
